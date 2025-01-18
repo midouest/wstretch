57fff8202f5926a26da1450f6853c2ef458607f8
Robin <midouesterner@gmail.com>
2025-01-18 05:57:14 -0600

    emmylua code formatting

--- wstretch.lua
+++ wstretch.lua
@@ -1,24 +1,24 @@
 -- w/stretch
 -- time-stretching with wtape
 function init()
-    clock.run(function()
-        while true do
-            clock.sleep(1 / 15)
-            redraw()
-        end
-    end)
+  clock.run(function()
+    while true do
+      clock.sleep(1/15)
+      redraw()
+    end
+  end)
 
-    norns.crow.loadscript("wstretch.lua")
+  norns.crow.loadscript("wstretch.lua")
 end
 
-function key(n, z) end
+function key(n,z) end
 
-function enc(n, d) end
+function enc(n,d) end
 
-needs_redraw = true
+needs_redraw=true
 function redraw()
-    if not needs_redraw then return end
-    needs_redraw = false
-    screen.clear()
-    screen.update()
+  if not needs_redraw then return end
+  needs_redraw=false
+  screen.clear()
+  screen.update()
 end
